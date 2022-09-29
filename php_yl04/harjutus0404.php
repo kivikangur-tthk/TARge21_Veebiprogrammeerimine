edeb30a35749f97c571fc596a6d781519fc76634
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2022-09-29 13:49:19 +0300

    Add indexes for exercises.

---
+++ php_yl04/harjutus0404.php
@@ -0,0 +1,15 @@
+<pre>
+Ülesanne 0404
+
+
+Tee vorm &lt;form&gt; tagiga kangelase andmete sisestamiseks. Pange kogu vorm ka tabelisse. Paigutage lahtrite kirjelduse esimesse veergu ja lahtrid teise veergu. Nagu on näidatud siin. http://students.tmk.edu.ee/php/naited/user_database/insert_user.php
+Nimi (&lt;input type=text&gt;)
+Klass (&lt;select&gt;, &lt;options&gt;)
+Rass (&lt;select&gt;, &lt;options&gt;)
+Sugu (&lt;input type=radio&gt;)
+Lemmikloom kaasas (&lt;input type=checkbox&gt;)
+Kirjeldus (&lt;textarea&gt;&lt;/textarea&gt;)
+Submit nupp &lt;input type=submit&gt;
+Vorm kinni.    
+</pre>
+<?php
---
+++ php_yl04/harjutus0405.php
@@ -0,0 +1,9 @@
+<pre>
+Ülesanne 0405
+
+Genereeri kahe FOR tsükkliga 10x10 korrutustabel.
+Korrutustabel peab olema tabeli sees.
+Korrutustabelis peab olema esimesel real ja veerul numbrid 1-10.
+Teisel real esimese rea ja veeru korrustis jne.
+</pre>
+<?php
