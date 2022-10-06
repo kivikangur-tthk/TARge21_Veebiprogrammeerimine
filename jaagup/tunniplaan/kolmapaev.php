415e661d9eee39f1ee7935b15e793d29682c25d2
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2022-10-06 11:45:17 +0300

    Add tunniplaan solution

---
+++ jaagup/tunniplaan/kolmapaev.php
@@ -0,0 +1,19 @@
+<?php require("p2is.php"); ?>
+
+    <h2>Kolmapäev</h2>
+
+    <p>
+
+    <ol>
+
+        <li>Matemaatika</li>
+
+        <li>Emakeel</li>
+
+        <li>Kehaline kasvatus</li>
+
+    </ol>
+
+    </p>
+
+<?php require("jalus.php"); ?>
