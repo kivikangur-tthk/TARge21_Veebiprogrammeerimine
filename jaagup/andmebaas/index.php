04cd26974931e9677762dd10f0a4935af0f9de05
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2022-10-13 11:45:52 +0300

    Update index to show links on each topic

--- jaagup/andmebaas/index.php
+++ jaagup/andmebaas/index.php
@@ -3,25 +3,69 @@ require("dbConfig.php"); // hoida väljaspool veebist kättesaadavat teekonda
 if (isset($user) && (isset($server)) && (isset($password)) && (isset($database))) {
     $yhendus = new mysqli($server, $user, $password, $database);
 }
-$kask = $yhendus->prepare("SELECT id, pealkiri, sisu FROM lehed");
-$kask->bind_result($id, $pealkiri, $sisu);
-$kask->execute();
 ?>
-    <!doctype html>
-    <html>
-    <head>
-        <title>Teated lehel</title>
-    </head>
-    <body>
-    <h1>Teadete loetelu</h1>
+<!doctype html>
+<html>
+<head>
+    <title>Teated lehel</title>
+    <style>
+        #menyykiht {
+            float: left;
+            padding-right: 30px;
+        }
+
+        #sisukiht {
+            float: left;
+        }
+
+        #jalusekiht {
+            clear: left;
+        }
+    </style>
+    <meta charset="utf-8"/>
+</head>
+<body>
+<div id="menyykiht">
+    <h2>Teated</h2>
+    <ul>
+        <?php
+        $kask = $yhendus->prepare("SELECT id, pealkiri FROM lehed");
+        $kask->bind_result($id, $pealkiri);
+        $kask->execute();
+        while ($kask->fetch()) {
+            echo "<li><a href='?id=$id'>" .
+                htmlspecialchars($pealkiri) . "</a></li>";
+        }
+        ?>
+    </ul>
+</div>
+<div id="sisukiht">
     <?php
-    while ($kask->fetch()) {
-        echo "<h2>" . htmlspecialchars($pealkiri) . "</h2>";
-        echo "<div>" . htmlspecialchars($sisu) . "</div>";
+    if (isset($_REQUEST["id"])) {
+        $kask = $yhendus->prepare("SELECT id, pealkiri, sisu FROM lehed
+                          WHERE id=?");
+        //Kysim2rgi asemele pannakse aadressiribalt tulnud id,
+        ////eeldatakse, et ta on tyybist integer (i).
+        /// //(double - d, string - s)
+        $kask->bind_param("i", $_REQUEST["id"]);
+        $kask->bind_result($id, $pealkiri, $sisu);
+        $kask->execute();
+        if ($kask->fetch()) {
+            echo "<h2>" . htmlspecialchars($pealkiri) . "</h2>";
+            echo htmlspecialchars($sisu);
+        } else {
+            echo "Vigased andmed.";
+        }
+    } else {
+        echo "Tere tulemast avalehele! Vali men&uuml;&uuml;st sobiv teema.";
     }
     ?>
-    </body>
-    </html>
+</div>
+<div id="jalusekiht">
+    Lehe tegi Jaagup
+</div>
+</body>
+</html>
 <?php
 $yhendus->close();
 ?>
---
+++ jaagup/andmebaas/teated.php
@@ -0,0 +1,27 @@
+<?php
+require("dbConfig.php"); // hoida väljaspool veebist kättesaadavat teekonda
+if (isset($user) && (isset($server)) && (isset($password)) && (isset($database))) {
+    $yhendus = new mysqli($server, $user, $password, $database);
+}
+$kask = $yhendus->prepare("SELECT id, pealkiri, sisu FROM lehed");
+$kask->bind_result($id, $pealkiri, $sisu);
+$kask->execute();
+?>
+    <!doctype html>
+    <html>
+    <head>
+        <title>Teated lehel</title>
+    </head>
+    <body>
+    <h1>Teadete loetelu</h1>
+    <?php
+    while ($kask->fetch()) {
+        echo "<h2>" . htmlspecialchars($pealkiri) . "</h2>";
+        echo "<div>" . htmlspecialchars($sisu) . "</div>";
+    }
+    ?>
+    </body>
+    </html>
+<?php
+$yhendus->close();
+?>
