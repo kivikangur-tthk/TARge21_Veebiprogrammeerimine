431cdaa21c197f746b9bbc9b8ed683ffe97ab13e
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2022-11-10 16:41:08 +0200

    Loo leht Laulu lisamiseks ja ansambli lisamiseks

---
+++ jaagup/andmebaas/ansamblid/lisamine.php
@@ -0,0 +1,31 @@
+<?php
+require("../dbConfig.php");
+global $yhendus;
+if (!empty($_REQUEST["uuepealkiri"])) {
+    $kask = $yhendus->prepare("INSERT INTO ansamblid(ansamblinimi,kommentaarid) VALUES(?,' ')");
+    $kask->bind_param("s", $_REQUEST["uuepealkiri"]);
+    $kask->execute();
+    echo $yhendus->error;
+    header("Location: $_SERVER[PHP_SELF]");
+    $yhendus->close();
+    exit();
+}
+?>
+    <!doctype html>
+    <html lang="et">
+    <head>
+        <title>Ansambli lisamine</title>
+    </head>
+    <body>
+    <h1>Ansambel</h1>
+    <form action="?">
+        <label>Uue ansambli nimi:
+            <input type="text" name="uuepealkiri"/>
+        </label>
+        <input type="submit" value="Lisa ansambel"/>
+    </form>
+    </body>
+    </html>
+<?php
+$yhendus->close();
+?>
